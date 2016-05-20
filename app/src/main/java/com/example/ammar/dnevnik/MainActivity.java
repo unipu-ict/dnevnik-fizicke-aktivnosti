1a4a01d95a58c4618e4f867b79728bfd6f4e1105
Josip Bošnjak <jbosnjak@unipu.hr>
2016-05-20 21:42:47 +0200

    pocetna slika i background

--- app/src/main/java/com/example/ammar/dnevnik/MainActivity.java
+++ app/src/main/java/com/example/ammar/dnevnik/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.ammar.dnevnik;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     // testni komentar
 
@@ -21,12 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
