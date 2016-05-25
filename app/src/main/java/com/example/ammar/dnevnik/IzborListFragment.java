d0d98db21790794c00767bd64bf5ce703fab7b7d
Josip Bošnjak <jbosnjak@unipu.hr>
2016-05-25 15:42:05 +0200

    Vraćeno stanje

--- app/src/main/java/com/example/ammar/dnevnik/IzborListFragment.java
+++ app/src/main/java/com/example/ammar/dnevnik/IzborListFragment.java
@@ -1,16 +1,7 @@
 package com.example.ammar.dnevnik;
 
-import android.app.Activity;
 import android.app.ListFragment;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -18,55 +9,11 @@ import java.util.ArrayList;
  * Created by acer on 23.5.2016..
  */
 public class IzborListFragment extends ListFragment {
-    OnSadrzajSelectedListener mCallback;
-
-
-    public interface OnSadrzajSelectedListener {
-
-        public void onIzborSelected(int position);
-    }
-
+    private ArrayList<Izbor> mIzbori;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
-
-        int layout = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
-                android.R.layout.simple_list_item_activated_1 : android.R.layout.simple_list_item_1;
-
-
-        setListAdapter(new ArrayAdapter<String>(getActivity(), layout, Izbor.Sadrzaj));
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-
-        if (getFragmentManager().findFragmentById(R.id.izbor) != null) {
-            getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        }
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-
-
-        try {
-            mCallback = (OnSadrzajSelectedListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + "Mora se implementirati sadrzaj");
-        }
-    }
-
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-
-        mCallback.onIzborSelected(position);
-
-
-        getListView().setItemChecked(position, true);
+        getActivity().setTitle(R.string.izbor_naslov);
+        mIzbori = Izbornik.get(getActivity()).getIzbor();
     }
 }
